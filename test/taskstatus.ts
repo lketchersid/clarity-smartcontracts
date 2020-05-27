f97d817b2c10f3ff4d0bd2cb58d51f58cbd5e8a2
Larry Ketchersid <ketchersid@sbcglobal.net>
2020-05-27 11:14:52 -0500

    initial test

---
+++ test/taskstatus.ts
@@ -0,0 +1,33 @@
+import { Client, Provider, ProviderRegistry, Result } from "@blockstack/clarity";
+import { assert } from "chai";
+
+describe("task status contract test suite", () => {
+  let taskStatusClient: Client;
+  let provider: Provider;
+
+  before(async () => {
+    provider = await ProviderRegistry.createProvider();
+    taskStatusClient = new Client("SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB.taskstatus", "taskstatus", provider);
+  });
+
+  it("should have a valid syntax", async () => {
+    await taskStatusClient.checkContract();
+  });
+
+  describe("deploying an instance of the contract", () => {
+    before(async () => {
+      await taskStatusClient.deployContract();
+    });
+
+    it("should return task status list, async () => {
+      const query = taskStatusClient.createQuery({ method: { name: "get-task-status", args: [] } });
+      const returns = await taskStatusClient.submitQuery(query);
+      const result = Result.unwrapString(returns);
+      assert.equal(result, "hello world");
+    });
+
+
+  after(async () => {
+    await provider.close();
+  });
+});
